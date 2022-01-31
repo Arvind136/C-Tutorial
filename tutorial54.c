c13f3ee3fe468b267f718ed1219cc8f39f950151
Arvind136 <90745235+Arvind136@users.noreply.github.com>
2022-01-31 13:37:17 +0530

    Add files via upload

---
+++ tutorial54.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+ 
+ 
+
+// int main()
+// {
+//     int *ptr=(int *)malloc(5*sizeof(int));
+//     ptr[0]=1;
+//     ptr[1]=2;
+//     ptr[2]=3;
+//     ptr[3]=4;
+//     free(ptr);
+//     printf("%d",ptr[3]);
+    
+//     return 0;
+// }
